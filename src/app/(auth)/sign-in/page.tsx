368cedb646b64c7bc9078f1daf1b17f647fd4861
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2026-01-18 16:57:34 +0530

    email auth done

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import SignIn from '@/app/_components/authComponents/sign-in'
+import React from 'react'
+
+const Page = () => {
+    return (
+        <SignIn />
+    )
+}
+
+export default Page
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import SignUp from '@/app/_components/authComponents/sign-up'
+import React from 'react'
+
+
+const Page = () => {
+    return (
+        <SignUp />
+    )
+}
+
+export default Page
